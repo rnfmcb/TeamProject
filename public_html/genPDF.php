e1c312c720ec9b018c24ed9574b61c75d404a3e2
Tim Schisler <tim-schisler@users.noreply.github.com>
2019-04-27 15:29:06 -0500

    Webhost build 4-27-2019
    Prepares PDF generation for accepting data from database.
    Revamps index page links.
    Adds a new DBops page to separate DB build functions from transcript functions.

--- public_html/genPDF.php
+++ public_html/genPDF.php
@@ -1,41 +1,57 @@
 <?php
 require '../php/FPDF/fpdf.php';
-
 class PDF extends FPDF
 {
 // Page header
 function Header()
 {
-    // Arial bold 15
-    $this->SetFont('Arial','B',15);
-    // Move to the right
+    // Arial bold 24
+    $this->SetFont('Arial','B',24);
+    // Centers header
     $this->Cell(80);
-    // Title
+    // Title text
     $this->Cell(30,10,'UMSL Enrichment Transcript',0,0,'C');
-    // Line break
+    // Line break to start non-header
     $this->Ln(20);
 }
-
-// Page footer
-function Footer()
-{
-    // Position at 1.5 cm from bottom
-    $this->SetY(-15);
-    // Arial italic 8
-    $this->SetFont('Arial','I',8);
-    // Page number
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 }
+//$SSOID = $POST["student"];
+//Array of student data passed by SSOID key
+$passedArray=array(
+    array('Name', 'Degree', 'Date', 'Title', 'Desc', 'Cat', '4'),
+    array('Name', 'Degree', 'Date', 'Title', 'Desc', 'Cat', '4'),
+    array('Name', 'Degree', 'Date', 'Title', 'Desc', 'Cat', '4'));
+$DASize = count($passedArray);
+//creates printable array filled with empty strings, filled in later loop
+$printingArray=array_fill(0, $DASize-1, array_fill(0, 4, ""));
+$stdName= $passedArray[0][0];
+$stdDegree = $passedArray[0][1];
+//Array-filling loop
+//Might be unnecesary, depending on how the initial array works
+//This is just asserting control over the data
+for($i=0; $i< $DASize;$i++){
+    //Event Date
+    $printingArray[$i][0] = $passedArray[$i][2];
+    //Event Title
+    $printingArray[$i][1] = $passedArray[$i][3];
+    //Event Description
+    $printingArray[$i][2] = $passedArray[$i][4];
+    //Event Category
+    $printingArray[$i][3] = $passedArray[$i][5];
+    //Hours spent on event
+    $printingArray[$i][4] = $passedArray[$i][6];
 }
 
-// Instanciation of inherited class
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-    $pdf->Cell(0,10,'Printing line number '.$i,0,1);
+$pdf->SetFont('Courier','',12);
+$pdf->Cell(0,10, 'Name: ' . $stdName,0,1);
+$pdf->Cell(0,10, 'Degree: ' . $stdDegree,0,1);
+//Loop prints every entry in the array
+//Text is tentatively set to wrap
+//If it doesn't work, FPDF's documentation will help
+for($i=0; $i< $DASize; $i++)
+    $pdf->MultiCell(0,10, $printingArray[$i][0] .' | ' .$printingArray[$i][1] .' | ' .$printingArray[$i][2] .' | ' .$printingArray[$i][3] .' | ' .$printingArray[$i][4],0,1);
 $pdf->Output();
-?>
-
 ?>
